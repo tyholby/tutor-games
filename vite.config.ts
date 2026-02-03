2f6ee5d19e196c0c0197fb8d1e2c5e0da6c90e2a
Ty Holbrook <tytyholbrook@gmail.com>
2026-02-03 03:25:34 -0700

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === 'production' ? '/tutor-games/' : '/',
 	plugins: [
 		vue(),
 		vueDevTools(),
